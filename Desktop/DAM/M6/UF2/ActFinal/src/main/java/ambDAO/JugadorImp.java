dea4bd4738159f71c2d1995191dd5b483da0adf6
vpenyarando <adam22vpenarando@inslaferreria.cat>
2024-02-08 16:33:00 +0100

    implementación básica de JugadorDAO

--- Desktop/DAM/M6/UF2/ActFinal/src/main/java/ambDAO/JugadorImp.java
+++ Desktop/DAM/M6/UF2/ActFinal/src/main/java/ambDAO/JugadorImp.java
@@ -1,5 +1,51 @@
 package ambDAO;
 
-public class JugadorImp {
+import org.hibernate.*;
+import model.*;
+import java.util.List;
+
+public class JugadorImp implements JugadorDAO {
+
+    private SessionFactory sf;
+
+    public JugadorImp(SessionFactory sessionFactory) {
+        this.sf = sessionFactory;
+    }
+
+    @Override
+    public void guardarJugador(Jugador jugador) {
+        Transaction t = null;
+        try (Session s = sf.openSession()) {
+            t = s.beginTransaction();
+            s.save(jugador);
+            t.commit();
+        } catch (Exception e) {
+            if (t != null) {
+                t.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public Jugador obtenerJugador(Long id) {
+        try (Session s = sf.openSession()) {
+            return s.get(Jugador.class, id);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public List<Jugador> obtenerTodosLosJugadores() {
+        try (Session s = sf.openSession()) {
+            return s.createQuery("FROM Jugador", Jugador.class).list();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
 }
+
