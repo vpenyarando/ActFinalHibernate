9669de824d1dedc97436923ada024b119d5404bb
vpenyarando <adam22vpenarando@inslaferreria.cat>
2024-02-08 15:48:40 +0100

    creada entitat Jugador, metodes y variables a Jugador.java

--- Desktop/DAM/M6/UF2/ActFinal/src/main/java/model/Jugador.java
+++ Desktop/DAM/M6/UF2/ActFinal/src/main/java/model/Jugador.java
@@ -1,5 +1,43 @@
 package model;
 
+import javax.persistence.*;
+
+@Entity
 public class Jugador {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String nombre;
+    private String color;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Jugador [id=" + id + ", nombre=" + nombre + ", color=" + color + "]";
+    }
 }
